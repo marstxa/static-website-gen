4422a982a7b138f188aa8cab7ee1689551b10c8b
marstxa <marstxa@gmail.com>
2026-08-05 22:18:04 +0100

    Add TextNode to HTMLNode conversion logic
    Implements a conversion function to transform various TextNode types into their corresponding HTML leaf nodes.
    
    Includes comprehensive unit tests to ensure correct handling of text, formatting styles, links, and images, while providing robust error handling for unsupported node types.

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -47,3 +47,7 @@ class TestHTMLNode(unittest.TestCase):
             parent_node.to_html(),
             "<div><span><b>grandchild</b></span></div>",
         )
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,10 @@
 from enum import Enum
 
+from htmlnode import HTMLNode, LeafNode, ParentNode
+
 
 class TextType(Enum):
+    TEXT = "text"
     BOLD = "****"
     ITALIC = "__"
     CODE = "``"
@@ -20,3 +23,25 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.name.lower()}, {self.url})"
+
+
+# Helper function
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+
+    match text_node.text_type:
+        case TextType.TEXT:
+            leaf_node = LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            leaf_node = LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            leaf_node = LeafNode("i", text_node.text)
+        case TextType.CODE:
+            leaf_node = LeafNode("code", text_node.text)
+        case TextType.LINK:
+            leaf_node = LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            leaf_node = LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception("TextNode does not have any of the values within TextType")
+
+    return leaf_node
